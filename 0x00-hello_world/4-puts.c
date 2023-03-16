df9e33771085a56e48276634ef79b1212b4d59d0
Yassin Elotmani <yassineothmani9@gmail.com>
2023-03-16 12:18:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - a program that prints a line with puts function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	put("Programming is like building a multilingual puzzle\n");
+	(return = 0);
+}
